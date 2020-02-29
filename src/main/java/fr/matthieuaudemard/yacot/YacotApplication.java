0f4c49745923bce8ea96e647904a0b93ba42711b
matthieuaudemard <matthieu.audemard@gmail.com>
2020-02-29 23:44:22 +0100

    initial commit

---
+++ src/main/java/fr/matthieuaudemard/yacot/YacotApplication.java
@@ -0,0 +1,13 @@
+package fr.matthieuaudemard.yacot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YacotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YacotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/matthieuaudemard/yacot/YacotApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.matthieuaudemard.yacot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YacotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
